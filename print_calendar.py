77732bd84124c8ec4c35e409c3148f02d5a7e9d9
Jordan <101244802+jabu9@users.noreply.github.com>
2023-05-05 20:39:44 -0400

    Create print_calendar.py

---
+++ print_calendar.py
@@ -0,0 +1,8 @@
+# Print Calendar
+
+import calendar
+
+year =int( input("Enter the year of the required calendar "))
+month = int( input("Enter the month of the required calendar "))
+
+print(calendar.month(year,month))                                                         
